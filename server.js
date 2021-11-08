3c5178d9b5f1c19c88de7aad27d87347afe4ff57
“liujie-Leo” <ljdhxd@qq.com>
2021-11-08 10:35:49 +0800

    a

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ let users = [
   { id: 2, name: "Jerry" },
   { id: 3, name: "Jersry" },
   { id: 4, name: "Jers2ry" },
-  { id: 5, name: "Jers2ry" },
 ];
 
 let server = http.createServer((req, res) => {
